bfd1033f831f2d1c29124f140fe46eb97d967e82
Pawandeep Singh <sploiteye@gmail.com>
2021-07-28 12:05:15 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,22 +1,56 @@
 #!/usr/bin/env bash
 clear
+
+version="sz-1.1-se"
+python="python3"
+johntheripper="john"
+
+tput setaf 4;
 echo """
 █▀█ █▀▀ █▀█ █░█ █ █▀█ █▀▀ █▀▄▀█ █▀▀ █▄░█ ▀█▀ █▀
 █▀▄ ██▄ ▀▀█ █▄█ █ █▀▄ ██▄ █░▀░█ ██▄ █░▀█ ░█░ ▄█
 """
-echo "sz-1.0-se"
+tput sgr0;
+tput setaf 6;
+echo $version
+tput sgr0;
 echo ""
 sleep 2
 echo "Installing required tools..."
 echo ""
-sudo apt install john -y # Instal JohntheRipper
-sudo apt install python3 -y # Installing Python3
+tput bold;
+sudo apt install john -y &> /dev/null # Instal JohntheRipper
+sudo apt install python3 -y &> /dev/null # Installing Python3
 clear
-printf "-" && python3 --version
-sleep 1.0
-printf "-" && john
-sleep 1.0
-clear
-echo "- Required tools are installed"
-echo "- Running script"
-python3 main.py
+tput sgr0;
+
+dpkg -s $python &> /dev/null
+
+if [ $? -eq 0 ]; then
+  tput setaf 2;
+  echo "- Python3 is installed"
+  py=true
+  sleep 1.5
+  tput sgr0;
+fi
+dpkg -s $johntheripper &> /dev/null
+
+if [ $? -eq 0 ]; then
+  tput setaf 2;
+  echo "- John & zip2john is installed."
+  jn=true
+  sleep 1.0
+  tput sgr0;
+fi
+
+if [ "$py" = "true" ] && [ "$jn" == "true" ]; then
+  tput setaf 3;
+  echo "- Running script..."
+  sleep 1.2
+  python3 main.py
+else
+  tput setaf 1;
+  echo "- Python3 is not installed"
+  echo "- John & zip2john is not installed"
+  tput sgr0;
+fi
